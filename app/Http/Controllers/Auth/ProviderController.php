262224a219c7f2bacc688c9c3f00fb824c5991a3
yasunori1005 <suzukiyasunori1005@gmail.com>
2023-04-12 07:43:09 +0900

    first commit

---
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
+class ProviderController extends Controller
+{
+    public function redirect($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+    public function callback($provider)
+    {
+        $SocialUser = Socialite::driver($provider)->user();
+        $user = User::updateOrCreate([
+            'provider_id' => $SocialUser->id,
+            'provider' => $provider
+        ], [
+            'name' => $SocialUser->name,
+            'username' => $SocialUser->nickname,
+            'email' => $SocialUser->email,
+            'provider_token' => $SocialUser->token,
+        ]);
+
+        Auth::login($user);
+        return redirect('/dashboard');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\Auth\ProviderController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/auth/{provider}/redirect',[ProviderController::class,'redirect']);
+
+Route::get('/auth/{provider}/callback', [ProviderController::class,'callback']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
